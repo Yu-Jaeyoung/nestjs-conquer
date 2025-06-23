a62decaea529c80776ff26b3f11b3f0957731127
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-06-23 21:24:45 +0900

    nestjs project generate

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPost(): Post {
+    return {
+      author: 'jaeyoung',
+      title: 'hello jaeyoung',
+      content: 'hello',
+      likeCount: 10000000,
+      commentCount: 100000,
+    };
+  }
+}
